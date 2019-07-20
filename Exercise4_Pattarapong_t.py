0d246828b272fb5818ef667d2f98dbe6c71adc82
Pattaraponggg <52350454+Pattaraponggg@users.noreply.github.com>
2019-07-20 20:20:28 +0700

    Add files via upload
    นายภัทรพงษ์ ทองย้อย

---
+++ Exercise4_Pattarapong_t.py
@@ -0,0 +1,10 @@
+a="---You Score---"
+print(a)
+b="Foundation English:10"
+print(b)
+c="Geueral Business:20"
+print(c)
+d="Introduction to Computer Systems:30"
+print(d)
+e="Computer Programming:40"
+print(e)
