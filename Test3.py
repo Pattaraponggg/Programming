ef0393c4fe2cfa6cf90af496fd821b1ea4cfc947
Pattaraponggg <52350454+Pattaraponggg@users.noreply.github.com>
2019-09-14 19:08:25 +0700

    Add files via upload

---
+++ Test3.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+#3.จงเขียนโปรแกรมวนซ้ำโดยบังคับใช้คำสั่ง while ให้แสดงคำว่า"Hello"จำนวน ๕ บรรทัด
+a=0
+n=5
+while  a < n :
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    print("Hello")
+    a +=5
+    
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
